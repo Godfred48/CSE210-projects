e89632b1ec0f6742127f7d9ec696a980678b4c54
Godfred48 <godfredagbogah48@gmail.com>
2026-01-09 21:23:44 +0000

    Third Exercise

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Welcome To Magic Number Guess?");
+        //generate a random number in the system for user to guess right.
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int userGuess;
+        do
+        {
+        Console.WriteLine("What is your guess?");
+        userGuess = int.Parse(Console.ReadLine());
+        if (userGuess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if(userGuess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed right!");
+        }
+        }  while (magicNumber != userGuess);
+        
     }
 }
